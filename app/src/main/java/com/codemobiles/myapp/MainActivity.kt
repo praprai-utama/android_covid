b97de47767855e03b28717cef79d3fc4f5655ef9
iBlurBlur <tanakorn.ngam@gmail.com>
2020-06-08 16:04:55 +0700

    add easypref. Shared Preferences lib

--- app/src/main/java/com/codemobiles/myapp/MainActivity.kt
+++ app/src/main/java/com/codemobiles/myapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.codemobiles.myapp
 
+import android.content.ContextWrapper
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.pixplicity.easyprefs.library.Prefs
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Prefs.Builder()
+            .setContext(this)
+            .setMode(ContextWrapper.MODE_PRIVATE)
+            .setPrefsName(packageName)
+            .setUseDefaultSharedPreference(true)
+            .build()
+
+
         login_button.setOnClickListener {
             val username = username_edittext.text.toString()
             val password = password_edittext.text.toString()
+
+            Prefs.putString("USERNAME", username)
+            Prefs.putBoolean("IS_LOGIN", true)
 
             Toast.makeText(applicationContext, username + password , Toast.LENGTH_SHORT).show()
         }
