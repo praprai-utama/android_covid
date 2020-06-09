8f0b150974f388b9329a62eb3cb0cbbf161f833a
iBlurBlur <tanakorn.ngam@gmail.com>
2020-06-09 09:55:54 +0700

    no actionbar, setup viewbinding, create constants

---
+++ app/src/main/java/com/codemobiles/myapp/Constants.kt
@@ -0,0 +1,7 @@
+package com.codemobiles.myapp
+
+// top level
+
+
+const val PREF_USERNAME = "USERNAME"
+const val PREF_IS_LOGIN = "IS_LOGIN"
--- app/src/main/java/com/codemobiles/myapp/MainActivity.kt
+++ app/src/main/java/com/codemobiles/myapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.content.ContextWrapper
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.codemobiles.myapp.databinding.ActivityMainBinding
 import com.pixplicity.easyprefs.library.Prefs
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Prefs.Builder()
             .setContext(this)
@@ -19,16 +24,21 @@ class MainActivity : AppCompatActivity() {
             .setUseDefaultSharedPreference(true)
             .build()
 
+        setupEventWidget()
+    }
 
-        login_button.setOnClickListener {
-            val username = username_edittext.text.toString()
-            val password = password_edittext.text.toString()
+    private fun setupEventWidget() {
+        binding.loginButton.setOnClickListener {
+            val username = binding.usernameEdittext.text.toString()
+            val password = binding.passwordEdittext.text.toString()
 
-            Prefs.putString("USERNAME", username)
-            Prefs.putBoolean("IS_LOGIN", true)
+            Prefs.putString(PREF_USERNAME, username)
+            Prefs.putBoolean(PREF_IS_LOGIN, true)
 
             Toast.makeText(applicationContext, username + password , Toast.LENGTH_SHORT).show()
         }
+
+        binding.usernameEdittext.setText(Prefs.getString(PREF_USERNAME, ""))
     }
 }
 
