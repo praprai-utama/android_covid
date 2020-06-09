1e1f1c7085de8116723250655ee651107b2a1ba0
iBlurBlur <tanakorn.ngam@gmail.com>
2020-06-09 13:05:18 +0700

    prepare stock design

--- app/src/main/java/com/codemobiles/myapp/ui/main/StockFragment.kt
+++ app/src/main/java/com/codemobiles/myapp/ui/main/StockFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.codemobiles.myapp.R
+import com.codemobiles.myapp.databinding.FragmentStockBinding
 
 
 class StockFragment : Fragment() {
+
+    private lateinit var binding: FragmentStockBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock, container, false)
+        binding = FragmentStockBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
