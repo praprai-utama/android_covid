dd9bdba54ebf885a27decfb9a389f375f7e00d45
iBlurBlur <tanakorn.ngam@gmail.com>
2020-06-10 15:57:12 +0700

    open camera and gallery

--- app/src/main/java/com/codemobiles/myapp/ProductFormActivity.kt
+++ app/src/main/java/com/codemobiles/myapp/ProductFormActivity.kt
@@ -8,11 +8,24 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import pl.aprilapps.easyphotopicker.ChooserType
+import pl.aprilapps.easyphotopicker.EasyImage
 
 class ProductFormActivity : AppCompatActivity() {
+
+    private lateinit var easyImage: EasyImage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_form)
+
+        easyImage = EasyImage.Builder(this)
+            .setChooserTitle("Pick media")
+            .setCopyImagesToPublicGalleryFolder(false)
+            .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+            .setFolderName("EasyImage sample")
+            .allowMultiple(true)
+            .build()
 
         checkRuntimePermission()
     }
@@ -25,7 +38,11 @@ class ProductFormActivity : AppCompatActivity() {
             ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport) {
                     if (report.areAllPermissionsGranted()) {
-                        //todo
+
+                        // testing
+                        //easyImage.openCameraForImage(this@ProductFormActivity)
+                        easyImage.openGallery(this@ProductFormActivity)
+
                     } else {
                         finish()
                     }
