4f4d435fc48a2ed866a8941f3a0dc009ce2255f1
iBlurBlur <tanakorn.ngam@gmail.com>
2020-06-11 11:24:09 +0700

    binding ui handler select image, toolbar *bug

--- app/src/main/java/com/codemobiles/myapp/ProductFormActivity.kt
+++ app/src/main/java/com/codemobiles/myapp/ProductFormActivity.kt
@@ -2,7 +2,9 @@ package com.codemobiles.myapp
 
 import android.Manifest
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.codemobiles.myapp.databinding.ActivityProductFormBinding
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -13,11 +15,14 @@ import pl.aprilapps.easyphotopicker.EasyImage
 
 class ProductFormActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityProductFormBinding
     private lateinit var easyImage: EasyImage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_form)
+
+        binding = ActivityProductFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         easyImage = EasyImage.Builder(this)
             .setChooserTitle("Pick media")
@@ -28,6 +33,43 @@ class ProductFormActivity : AppCompatActivity() {
             .build()
 
         checkRuntimePermission()
+        setupEventWidget()
+        setupToolbar()
+    }
+
+    private fun setupEventWidget() {
+        binding.camera.setOnClickListener(View.OnClickListener {
+            easyImage.openCameraForImage(this)
+        })
+
+        binding.gallery.setOnClickListener(View.OnClickListener {
+            easyImage.openGallery(this)
+        })
+
+        binding.productSubmit.setOnClickListener(View.OnClickListener {
+//            val byteArray: ByteArray? = file?.let { file ->
+//                covertByteArray(file)
+//            }
+//
+//            it.isEnabled = false
+//
+//            upload(
+//                binding.productEdittextName.text.toString(),
+//                binding.productEdittextPrice.text.toString(),
+//                binding.productEdittextStock.text.toString(),
+//                byteArray,
+//                file?.name
+//            )
+        })
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setOnClickListener {
+            finish()
+        }
     }
 
     private fun checkRuntimePermission() {
@@ -38,11 +80,7 @@ class ProductFormActivity : AppCompatActivity() {
             ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport) {
                     if (report.areAllPermissionsGranted()) {
-
-                        // testing
-                        //easyImage.openCameraForImage(this@ProductFormActivity)
-                        easyImage.openGallery(this@ProductFormActivity)
-
+                        //todo
                     } else {
                         finish()
                     }
